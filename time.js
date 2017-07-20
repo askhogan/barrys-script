941eabf24b9bc6ec711d8801b64ecda7f1a7900a
marcus petty-saphon <mpetty-saphon@vccp.com>
2017-07-20 17:16:47 +0100

    changing from set interval to set TImeout

--- time.js
+++ time.js
@@ -2,10 +2,9 @@ const moment = require('moment');
 const bookBarrys = require('./index.js');
 
 let now = moment().format('MMMM Do YYYY, h:mm:ss a')
-let timeUntilMidday = Math.abs(moment().diff(moment().hour(16).minute(50).second(0), 'milliseconds'));
+let timeUntilMidday = Math.abs(moment().diff(moment().hour(17).minute(07).second(0), 'milliseconds'));
 
-
-setInterval(() => {
+setTimeout(() => {
  bookBarrys();
   console.log('booked')
 }, timeUntilMidday)
